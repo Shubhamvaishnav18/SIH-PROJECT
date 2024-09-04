eaa9b73136b2d2261652fa81b4a54045d7f80098
shubhamvaishnav18 <shubhamvaishnav332@gmail.com>
2024-09-04 22:23:01 +0530

    bug solved

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotevn = require('dotenv');
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require('path');
 const checkUserAuth = require('./middleware/authMiddleware.js')
 const authRouter = require("./routes/authRoutes.js");
 const userRouter = require("./routes/userRoutes.js");
@@ -13,6 +14,12 @@ const app = express();
 dotevn.config();
 app.use(cors());
 connectDB()
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use(express.json());
 app.use(express.json()); // For parsing application/json
